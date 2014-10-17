9ee90755d16627b6e5ee47d2ccf60ac792089db9
Matias Fontanini <matias.fontanini@gmail.com>
2014-10-17 12:14:00 -0300

    Moved headers to include/tins.

---
+++ include/tins/config.h
@@ -0,0 +1,9 @@
+
+/* Define if the compiler supports basic C++11 syntax */
+#define HAVE_CXX11
+
+/* Have IEEE 802.11 support */
+#define HAVE_DOT11
+
+/* Have WPA2 decryption library */
+#define HAVE_WPA2_DECRYPTION
