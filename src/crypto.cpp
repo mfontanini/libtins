a0f75e10587d3f4a80fa94ef6b9a72b9cb25b10a
prasad24582 <prasad.24582@gmail.com>
2015-05-18 18:37:38 -0400

    Update crypto.cpp

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -271,14 +271,6 @@ SessionKeys::SessionKeys(const ptk_type &rptk, const bool &ccmp) {
     is_ccmp = ccmp; 
 }
 
-const ptk_type &SessionKeys::get_ptk() const {
-    return ptk;
-}
-
-bool SessionKeys::get_ccmp() const {
-    return is_ccmp;
-}
-
 SNAP *SessionKeys::ccmp_decrypt_unicast(const Dot11Data &dot11, RawPDU &raw) const {
     RawPDU::payload_type &pload = raw.payload();
     uint8_t MIC[16] = {0};
