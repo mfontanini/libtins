1ee13fb8acb86116f1b5a13097c86e52a5fb51a3
prasad24582 <prasad.24582@gmail.com>
2015-05-18 17:49:52 -0400

    Update crypto.h

--- include/tins/crypto.h
+++ include/tins/crypto.h
@@ -66,14 +66,18 @@ namespace Crypto {
             
             SessionKeys();
             SessionKeys(const RSNHandshake &hs, const pmk_type &pmk);
-            SessionKeys(const ptk_type &rptk, const bool &ccmp){
+            SessionKeys(const ptk_type &rptk, const bool &ccmp) {
                 std::copy( rptk.begin(), rptk.end(), ptk.begin());
                 is_ccmp = ccmp; 
             }
 
             SNAP *decrypt_unicast(const Dot11Data &dot11, RawPDU &raw) const;
-            ptk_type& get_ptk() const{return ptk;}
-            bool get_ccmp() const {return is_ccmp;}
+            ptk_type& get_ptk() const {
+                return ptk;
+            }
+            bool get_ccmp() const {
+                return is_ccmp;
+            }
 
         private:
             SNAP *ccmp_decrypt_unicast(const Dot11Data &dot11, RawPDU &raw) const;
