fa0b969f7a17691691d59de5f3c991f7b615968f
Jim Hague <jim@sinodun.com>
2016-08-31 12:45:02 +0100

    Correct typo preventing user buffer management for server TCP streams.

--- src/tcp_ip/stream.cpp
+++ src/tcp_ip/stream.cpp
@@ -258,7 +258,7 @@ void Stream::auto_cleanup_client_data(bool value) {
 }
 
 void Stream::auto_cleanup_server_data(bool value) {
-    auto_cleanup_client_ = value;
+    auto_cleanup_server_ = value;
 }
 
 void Stream::enable_ack_tracking() {
