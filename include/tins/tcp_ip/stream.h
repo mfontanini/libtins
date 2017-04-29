2c6ef2a5c05dce8553872fa65aa92a82956bed63
Matias Fontanini <matias.fontanini@gmail.com>
2017-04-29 09:56:26 -0700

    Update license date to 2017

--- include/tins/tcp_ip/stream.h
+++ include/tins/tcp_ip/stream.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Matias Fontanini
+ * Copyright (c) 2017, Matias Fontanini
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
